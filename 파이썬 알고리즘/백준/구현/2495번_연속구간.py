c81c08aabf343a8394d804325d68c6f8ca23fa8b
hyeonseo <gustj7038@gmail.com>
2024-12-27 23:04:13 +0900

    Create 2495번_연속구간.py

---
+++ 파이썬 알고리즘/백준/구현/2495번_연속구간.py
@@ -0,0 +1,24 @@
+def inspect_list(s):
+    flag = False
+    count = []  # 구간 길이
+
+    for i in range(len(s)):
+        if i + 1 < len(s):
+            if s[i + 1] == s[i]:  # 다음 값과 현재 값이 같으면
+                if not flag:  # 새로운 연속 구간 시작
+                    count.append(1)  # 구간 길이 초기화
+                else:
+                    count[-1] += 1  # 현재 구간간 길이 증가
+                flag = True
+            else:
+                flag = False
+    
+    if not count:  
+        print(1)
+    else:
+        print(max(count) + 1)  
+
+# 테스트 실행
+for _ in range(3):
+    s = list(map(int, str(input())))
+    inspect_list(s)
