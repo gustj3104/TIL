aaea4931ddb3ca800ebf200db6c4c4048db3fbc5
hyeonseo <gustj7038@gmail.com>
2025-01-23 11:42:45 +0900

    Create 7576번_토마토.py

---
+++ 파이썬 알고리즘/백준/그래프/7576번_토마토.py
@@ -0,0 +1,36 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+m, n = map(int, input().split())
+field = [list(map(int, input().split())) for _ in range(n)]
+
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+def bfs():
+    queue = deque()
+    total_days = 0
+
+    for i in range(n):
+        for j in range(m):
+            if field[i][j] == 1:
+                queue.append((i, j, 0))
+
+    while queue:
+        x, y, days = queue.popleft()
+        total_days = max(total_days, days)
+
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            
+            if 0 <= nx < n and 0 <= ny < m and field[nx][ny] == 0:
+                field[nx][ny] = 1
+                queue.append((nx, ny, days + 1))
+
+    for row in field:
+        if 0 in row:
+            return -1
+        
+    return total_days
+
+print(bfs())
