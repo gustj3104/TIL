b7a0cb34bb4a161c3e7b295a026a5ef5b536c0ed
hyeonseo <gustj7038@gmail.com>
2025-02-13 21:58:26 +0900

    Update 1598번_꼬리를 무는 숫자 나열.py

--- 파이썬 알고리즘/백준/사칙연산/1598번_꼬리를 무는 숫자 나열.py
+++ 파이썬 알고리즘/백준/사칙연산/1598번_꼬리를 무는 숫자 나열.py
@@ -1,20 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split()) # n, m 까지의 거리 구하기
-distance = 0
-if n > m :
-    n, m = m, n
+n, m = map(int, input().split()) 
+if n > m:
+    n, m = m, n  
 
-distance = ((m-1) % 4) + ((m-1) // 4) 
+row_distance = abs((m - 1) // 4 - (n - 1) // 4)
+col_distance = abs((m - 1) % 4 - (n - 1) % 4)
+distance = row_distance + col_distance
+
 print(distance)
-# while True:   
-#     if m == n:
-#         print(distance)
-#         break
-#     if m - 4 >= n:
-#         m -= 4
-#         distance += 1 #가로로 이동
-#     else:
-#         m += 1
-#         distance += 1 #세로로 이동
