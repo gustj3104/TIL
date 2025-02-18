f306d94f1da6fa01c808f7c927d8f67ecabd299d
hyeonseo <gustj7038@gmail.com>
2025-02-18 12:09:25 +0900

    Create: 백준 묹제풀이

---
+++ 파이썬 알고리즘/백준/문자열/4470번_줄번호.py
@@ -0,0 +1,6 @@
+n = int(input())
+sentence = []
+
+for i in range(1, n+1):
+    s = input()
+    print("%d. %s" %(i, s))
---
+++ 파이썬 알고리즘/백준/사칙연산/2010번_플러그.py
@@ -0,0 +1,11 @@
+n = int(input()) # 멀티탭 수
+
+result = 0
+for c in range(n):
+    plug = int(input()) # 각 멀티탭의 수용 가능한 플러그 수
+    if c == n - 1:
+        result += plug
+    else:
+        result += (plug - 1)
+
+print(result)
