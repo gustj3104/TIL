4bda632a84392936d440d2786f29b0a5423d2954
hyeonseo <gustj7038@gmail.com>
2025-02-23 17:03:56 +0900

    Create 1010번_다리 놓기.py

---
+++ 파이썬 알고리즘/백준/다이나믹 프로그래밍/1010번_다리 놓기.py
@@ -0,0 +1,9 @@
+import sys
+import math
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    bridge = math.factorial(m) // (math.factorial((m-n)) * math.factorial(n))
+    print(bridge)
