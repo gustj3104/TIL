a0a70253e12e9812cb588761b84c7c8b446fe22d
hyeonseo <gustj7038@gmail.com>
2025-02-27 19:51:39 +0900

    Create 25591번_푸앙이와 종윤이.py

---
+++ 파이썬 알고리즘/백준/수학/25591번_푸앙이와 종윤이.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+x, y = map(int, input().split())
+
+a = 100 - x
+b = 100 - y
+c = 100 - (a + b)
+d = a * b
+q = d // 100
+r = d % 100
+print(a, b, c, d, q, r)
+if d > 100:
+    print(c+q, r)
+else:
+    print(c, d)
