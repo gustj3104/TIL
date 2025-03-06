c12ca5246c797b0339c07f07bf5470db600b5798
hyeonseo <gustj7038@gmail.com>
2025-03-06 23:05:44 +0900

    Update 2061번_좋은 암호.py

--- 파이썬 알고리즘/백준/브루트포스 알고리즘/2061번_좋은 암호.py
+++ 파이썬 알고리즘/백준/브루트포스 알고리즘/2061번_좋은 암호.py
@@ -2,14 +2,11 @@ import sys
 input = sys.stdin.readline
 
 K, L = map(int, input().split())
-min_factor = K # 최소 소인수
-
-for i in range(2, int(K**0.5) + 1):
-    if K % i == 0:
-        min_factor = i
+for i in range(2, L):
+    if i * i > K:
         break
-
-if min_factor >= L:
-    print("GOOD")
-else:
-    print("BAD", min_factor)
+    if K % i == 0:
+        print("BAD", i)
+        exit(0)
+    
+print("GOOD")
