9f643ff52b9fd790d3beb81ec2361dd69bb982a5
hyeonseo <gustj7038@gmail.com>
2025-03-07 23:18:21 +0900

    Create 27541번_마지막 문자.py

---
+++ 파이썬 알고리즘/백준/문자열/27541번_마지막 문자.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+word = list(input().strip())
+if word[n-1] == 'G':
+    print("".join(word[:n-1]))
+else:
+    word.append("G")
+    print(word)
+    print("".join(word))
