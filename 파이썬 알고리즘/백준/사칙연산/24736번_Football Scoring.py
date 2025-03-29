5b486a756b11547879efa483f1da970a0204cc9a
hyeonseo <gustj7038@gmail.com>
2025-03-29 23:28:05 +0900

    Create 24736번_Football Scoring.py

---
+++ 파이썬 알고리즘/백준/사칙연산/24736번_Football Scoring.py
@@ -0,0 +1,6 @@
+T, F, S, P, C = map(int, input().split())
+v_team_score = T * 6 + F * 3 + S * 2 + P * 1 + C * 2
+T, F, S, P, C = map(int, input().split())
+h_team_score = T * 6 + F * 3 + S * 2 + P * 1 + C * 2
+
+print(v_team_score, h_team_score)
