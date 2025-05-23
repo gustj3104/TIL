7da0e67a59793f21acf283938f675a0f66a737ba
hyeonseo <gustj7038@gmail.com>
2025-05-23 22:35:37 +0900

    백준 - 그게 무슨 코드니 수정

--- 자바 알고리즘/boj_31495.java
+++ 자바 알고리즘/boj_31495.java
@@ -6,9 +6,20 @@ public class boj_31495 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
+        if (s.equals("") || s.equals("\"")) {
+            System.out.println("CE");
+            return;
+        }
+
         char[] word = s.toCharArray();
+
         if (word[0] == '"' && word[word.length - 1] == '"') {
-            System.out.println(s.substring(1, s.length()-1));
+            String out = s.substring(1, s.length() - 1);
+            if (out.equals("")) {
+                System.out.println("CE");
+            } else {
+                System.out.println(out);
+            }
         } else {
             System.out.println("CE");
         }
