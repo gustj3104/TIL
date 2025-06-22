889c0adbd3bbd2105903265680bb0ae6c97e3265
hyeonseo <gustj7038@gmail.com>
2025-06-22 16:59:58 +0900

    프로그래머스 - 오픈채팅방

---
+++ 자바 알고리즘/PG_오픈채팅방.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+  public String[] solution(String[] record) {
+    HashMap<String, String> map = new HashMap<String, String>();
+    List<String> answer = new ArrayList<>();
+
+    for (String r : record){
+      String[] m = r.split(" ");
+      String cmd = m[0];
+
+      if (cmd.equals("Enter") || cmd.equals("Change")){
+        String uid = m[1];
+        String name = m[2];
+        map.put(uid, name);
+      }
+    }
+
+    for(String r : record){
+      String[] m = r.split(" ");
+      String cmd = m[0];
+      String uid = m[1];
+
+      if (cmd.equals("Enter")){
+        answer.add(map.get(uid) + "님이 들어왔습니다.");
+      } else if(cmd.equals("Leave")) {
+        answer.add(map.get(uid) + "님이 나갔습니다.");
+      }
+    }
+
+
+
+    return answer.toArray(new String[0]);
+  }
+}
