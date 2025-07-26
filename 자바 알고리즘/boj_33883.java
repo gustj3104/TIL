1ba768147174249168e94d056d518d290b0c9f2c
hyeonseo <gustj7038@gmail.com>
2025-07-26 23:55:14 +0900

    백준 - Acentuación del idioma español refactor

--- 자바 알고리즘/boj_33883.java
+++ 자바 알고리즘/boj_33883.java
@@ -8,38 +8,34 @@ public class boj_33883 {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     char[] vowels = {'a', 'e', 'o', 'u', 'i'};
     String target = br.readLine();
-    StringBuffer sb = new StringBuffer(target);
-    String r_target = sb.reverse().toString();
+    StringBuilder sb = new StringBuilder(target);
+    String reversed = sb.reverse().toString();
 
-    char[] t = target.toCharArray();
-    char last = t[t.length - 1];
-    if (last == 'n' || last == 's') {
-      findSecondVowel(r_target, vowels);
-      System.exit(0);
+    char last = target.charAt(target.length() - 1);
+    if (last == 'n' || last == 's' || isVowel(last, vowels)) {
+      findSecondVowel(reversed, vowels);
+    } else {
+      findLastVowel(target, vowels);
     }
 
-    for (char v : vowels) {
-      if (last == v) {
-        findSecondVowel(r_target, vowels);
-        System.exit(0);
-      }
-    }
-
-    findLastVowel(target, vowels);
 
   }
 
-  public static void findSecondVowel(String target, char[] vowels) {
-    int flag = 0;
-    for (int i = 0; i < target.length(); i++) {
-      char s = target.charAt(i);
-      for (char c : vowels) {
-        if (s == c) {
-          flag++;
-          if (flag == 2) {
-            System.out.println(target.length() - i);
-            return;
-          }
+  private static boolean isVowel(char c, char[] vowels) {
+    for (char v : vowels) {
+      if (c == v) return true;
+    }
+    return false;
+  }
+
+  public static void findSecondVowel(String reversed, char[] vowels) {
+    int count = 0;
+    for (int i = 0; i < reversed.length(); i++) {
+      if (isVowel(reversed.charAt(i), vowels)) {
+        count++;
+        if (count == 2) {
+          System.out.println(reversed.length() - i);
+          return;
         }
       }
     }
@@ -48,16 +44,14 @@ public class boj_33883 {
 
   public static void findLastVowel(String target, char[] vowels) {
     for (int i = target.length() - 1; i >= 0; i--) {
-      char s = target.charAt(i);
-      for (char c : vowels) {
-        if (s == c) {
-          System.out.println(i + 1);
-          return;
-        }
+      if (isVowel(target.charAt(i), vowels)) {
+        System.out.println(i + 1);
+        return;
       }
     }
     System.out.println(-1);
   }
+
 }
 
 /*
